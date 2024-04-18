897e51f608c297c8e532197ab4ae37014d0b8267
Abdulrehmanabs <abdulrehmanabs786@gmail.com>
2024-04-18 10:45:42 +0500

    Update 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>welcome</h1>
+      <h1 className="text-center p-10 text-3xl font-bold hover:-translate-y-10 hover:translate-x-10  hover:scale-150 transition-all duration-500">
+        welcome
+      </h1>
     </>
   );
 }
